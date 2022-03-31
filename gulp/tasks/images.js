6b2e20fb3f57549fab2c481bd8d756a0515fc5c3
GruBoy <nazarogarishev333@gmail.com>
2022-03-31 16:32:38 +0300

    gulp

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -10,6 +10,19 @@ export const images = () =>{
             message: "error: <%= error.message %>"
         })
     ))
+    .pipe(app.plugins.newer(app.path.build.images))
+    .pipe(webp())
+    .pipe(app.gulp.dest(app.path.build.images))
+    .pipe(app.gulp.src(app.path.build.images))
+    .pipe(app.plugins.newer(app.path.build.images))
+    .pipe(imagemin({
+        progressive: true,
+        svgoPlugins: [{removeViewBox: falce}],
+        interlaced: true,
+        optimizationLevel: 3
+    }))
+    .pipe(app.gulp.dest(app.path.build.images))
+    .pipe(app.gulp.src(app.path.src.svg))
     .pipe(app.gulp.dest(app.path.build.images))
     .pipe(app.plugins.browsersync.stream())
 }
